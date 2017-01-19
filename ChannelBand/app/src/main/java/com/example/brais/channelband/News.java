a833fcb80203fbbedda95347051bb010fe6cafc0
BraisGonzlez <braisgi@gmail.com>
2017-01-19 18:26:56 +0100

    Funciona Fans(solo formulario, no hace nada) Gallery(Accede a fotos internet) multimedia History(Scroll realizado)

--- ChannelBand/app/src/main/java/com/example/brais/channelband/News.java
+++ ChannelBand/app/src/main/java/com/example/brais/channelband/News.java
@@ -7,11 +7,11 @@ import android.support.v7.app.AppCompatActivity;
  * Created by brais on 16/01/2017.
  */
 
-public class Home extends AppCompatActivity {
+public class News extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.news);
     }
 }
