ca9ec2501373f5c4d6dd80b8851d4a9f35fb9129
BraisGonzlez <braisgi@gmail.com>
2017-01-24 02:26:49 +0100

    Layouts: News->Empty History->Full Components->Full Show-> need utility Fans-> need utility Gallery-> Full Multimedia->Full ---------------------- Fragments Link social networks

--- ChannelBand/app/src/main/java/com/example/brais/channelband/Gallery.java
+++ ChannelBand/app/src/main/java/com/example/brais/channelband/Gallery.java
@@ -12,7 +12,7 @@ import com.bumptech.glide.Glide;
 
 public class Gallery extends AppCompatActivity {
 
-    private ImageView foto;
+    private ImageView foto,foto2,foto3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +20,23 @@ public class Gallery extends AppCompatActivity {
         setContentView(R.layout.gallery);
 
         foto = (ImageView) findViewById(R.id.foto);
+        foto2 = (ImageView) findViewById(R.id.foto2);
+        foto3 = (ImageView) findViewById(R.id.foto3);
+
         String URL = "http://www.orquestasdegalicia.es/img/portada/orquestas/id347portada.jpg";
+        String URL2 = "https://i.ytimg.com/vi/b_M783D35Ac/maxresdefault.jpg";
+        String URL3 = "https://i.ytimg.com/vi/Jlud9_rwIGg/maxresdefault.jpg";
+
+
         //PARA INSERTAR IMAGENES
         Glide.with(this)
                 .load(URL)
                 .into(foto);
+        Glide.with(this)
+                .load(URL2)
+                .into(foto2);
+        Glide.with(this)
+                .load(URL3)
+                .into(foto3);
     }
 }
--- ChannelBand/app/src/main/java/com/example/brais/channelband/History.java
+++ ChannelBand/app/src/main/java/com/example/brais/channelband/History.java
@@ -2,15 +2,30 @@ package com.example.brais.channelband;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * Created by brais on 12/01/2017.
  */
 
 public class History extends AppCompatActivity {
+
+    ImageView photo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
+
+        photo = (ImageView) findViewById(R.id.photo);
+
+        String URL = "http://www.orquestasdegalicia.es/img/portada/orquestas/id347portada.jpg";
+        //PARA INSERTAR IMAGENES
+
+        Glide.with(this)
+                .load(URL)
+                .into(photo);
     }
 }
